9c7e7e7c447ca0302939f256e6c7e1e5df9955c2
Onoja <jamesonojae@gmail.com>
2022-02-12 13:47:47 +0100

    working on the layout

--- src/Navbar/NavBar.js
+++ src/Navbar/NavBar.js
@@ -4,19 +4,39 @@ const NavBar = () => {
   const [isOpen, setIsOpen] = useState(false);
   return (
       <div className="Navbar">
-        <div className="nav-logo d-flex">
-          <span className="">DevLHB</span>
+        <div className="nav-logo">
+          <span className=""><img src="/assets/images/logo-cenam2.png"  alt=''/></span>
           {/*<span className="littleStyle">power by</span>*/}
         </div>
         <div className={`nav-items ${isOpen && "open"}`}>
           <a href="/home">Home</a>
-          <a href="/about">About</a>
-          <a href="/service">Service</a>
-          <a href="/contact">Contact</a>
+          <a ><span className="loginBtn"  data-toggle="modal" data-target="#myModal">Login</span></a>
         </div>
         <div className={`nav-toggle ${isOpen && "open"}`} onClick={() => setIsOpen(!isOpen)}>
           <div className="bar">
 
+          </div>
+        </div>
+        <div className="modal" id="myModal">
+          <div className="modal-dialog">
+            <div className="modal-content">
+
+
+              <div className="modal-header">
+                <h4 className="modal-title">Modal Heading</h4>
+                <button type="button" className="close" data-dismiss="modal">&times;</button>
+              </div>
+
+              <div className="modal-body">
+                Modal body..
+              </div>
+
+
+              <div className="modal-footer">
+                <button type="button" className="btn btn-danger" data-dismiss="modal">Close</button>
+              </div>
+
+            </div>
           </div>
         </div>
       </div>
--- src/components/home/SubHeader.js
+++ src/components/home/SubHeader.js
@@ -3,10 +3,103 @@ import './home.css';
 
 const SubHeader = () => {
   return (
-      <>
-        <div className="colorStyle">hi
+      <div className="sub-header">
+        <div className="firstLayout">
+          <div className="row">
+            <div className="col-md-6 subHeaderStyle">
+              Coinmena Trading for Every one
+              <p className="smallText">
+                Deployment long tail monetization strategy equity basic of conversion.
+                Supply chain freemium investor long tail agile development prototype validation influencer.
+              </p>
+              <div className="row">
+                <div className="col-md-6">
+                  <button className="btn btnColor">Join Our Trading PlatForm</button>
+                </div>
+
+
+              </div>
+            </div>
+            <div className="col-md-6">
+              <img src='/assets/images/bitcoin-image2.png' alt='' />
+            </div>
+
+              <div className="col-md-2"/>
+              <div className="col-md-8 sectionStyle">
+                <div className="card cardStyle">
+                  <div className="card-body">
+                    <input className="form-control inputStyle"/>
+                    <table className="table table-hover">
+                      <thead>
+                      <tr>
+                        <th>Firstname</th>
+                        <th>Lastname</th>
+                        <th>Email</th>
+                      </tr>
+                      </thead>
+                      <tbody>
+                      <tr>
+                        <td>John</td>
+                        <td>Doe</td>
+                        <td>john@example.com</td>
+                      </tr>
+                      <tr>
+                        <td>Mary</td>
+                        <td>Moe</td>
+                        <td>mary@example.com</td>
+                      </tr>
+                      <tr>
+                        <td>July</td>
+                        <td>Dooley</td>
+                        <td>july@example.com</td>
+                      </tr>
+                      <tr>
+                        <td>John</td>
+                        <td>Doe</td>
+                        <td>john@example.com</td>
+                      </tr>
+                      <tr>
+                        <td>Mary</td>
+                        <td>Moe</td>
+                        <td>mary@example.com</td>
+                      </tr>
+                      <tr>
+                        <td>July</td>
+                        <td>Dooley</td>
+                        <td>july@example.com</td>
+                      </tr>
+                      <tr>
+                        <td>John</td>
+                        <td>Doe</td>
+                        <td>john@example.com</td>
+                      </tr>
+                      <tr>
+                        <td>Mary</td>
+                        <td>Moe</td>
+                        <td>mary@example.com</td>
+                      </tr>
+                      <tr>
+                        <td>July</td>
+                        <td>Dooley</td>
+                        <td>july@example.com</td>
+                      </tr>
+                      <tr>
+                        <td>July</td>
+                        <td>Dooley</td>
+                        <td>july@example.com</td>
+                      </tr>
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-2"/>
+
+          </div>
         </div>
-      </>
+        <div className="secondLayout">hi
+        </div>
+      </div>
   );
 };
 
